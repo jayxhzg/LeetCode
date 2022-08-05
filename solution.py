1134bfafd1fcc174ea1faece1ed5e19dbd42572b
jayxhzg <79929261+jayxhzg@users.noreply.github.com>
2022-08-05 18:25:56 +0800

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int: 
+        '''
+        ###Algorithm(DP)###
+        Solution(target)
+        1.construct table and count(initial=0) to record the sub-result and result,respectively
+        2.check if the result of target is in the table
+        3.for number in nums:
+        4.  subTarget = target - number(if >= 0)
+        5.  if subTarget == 0:
+        6.      count++
+        7.  else:
+        8.      count += Solution(subTarget)
+        9.      save sub-result in table
+        10. return count
+        '''
+        table = {}
+        def Count(target):
+            count = 0
+            subCount = 0
+            if target in table:
+                return table[target]
+            for n in nums:
+                if n<=target:
+                    subTarget = target - n
+                    if subTarget == 0:
+                        count += 1
+                    else:
+                        subCount =Count(subTarget)
+                        count += subCount
+                        table[subTarget] = subCount
+            return count
+        return Count(target)
